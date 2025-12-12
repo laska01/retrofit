85c4c4ea1bce43e92083a0aa7a4e5f2569c3266b
laska01 <lifejanek@gmail.com>
2025-12-12 11:00:24 +0100

    jedno pytanie sie wyswietla

--- app/src/main/java/com/example/polaczenie/MainActivity.java
+++ app/src/main/java/com/example/polaczenie/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.polaczenie;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.List;
 
@@ -18,13 +18,29 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<pytanie> pytania;
+    TextView textviewPytanie;
+    RadioGroup radioGroup;
+     int  radioButtonid[] = new int[]{
+             R.id.radioButton,
+             R.id.radioButton2,
+             R.id.radioButton3
+    };
+     RadioButton radioButton_a;
+     RadioButton radioButton_b;
+     RadioButton radioButton_c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
+        textviewPytanie = findViewById(R.id.textviewTrescPytania);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton_a = findViewById(R.id.radioButton);
+        radioButton_b = findViewById(R.id.radioButton2);
+        radioButton_c = findViewById(R.id.radioButton3);
         //pobieramy dane z kad
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://my-json-server.typicode.com/laska01/polaczenie1/")
@@ -40,8 +56,9 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT);
                             return;
                         }
-                        List<pytanie> pytania = response.body();
+                        pytania = response.body();
                         Toast.makeText(MainActivity.this, pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
+                        wyswietlPytanie(0);
 
                     }
 
@@ -53,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+
+    }
+    private void wyswietlPytanie(int ktore){
+        pytanie pytanie = pytania.get(ktore);
+        textviewPytanie.setText(pytanie.getTrescPytania());
+        radioButton_a.setText(pytanie.getOdpa());
+        radioButton_b.setText(pytanie.getOdpb());
+        radioButton_c.setText(pytanie.getOdpc());
 
     }
 }
